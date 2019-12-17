72044113a0e5589457ce8e109033081851374f69
Shraddha3273 <58220358+Shraddha3273@users.noreply.github.com>
2019-12-17 19:00:55 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,7 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height) {
+    super(x,y,70,70)
+    this.image = loadImage("sprites/wood1.png")
+    }
+  }
+  
---
+++ Ground.js
@@ -0,0 +1,17 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("sprites/base.png")
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+     imageMode(CENTER);
+     image(this.image,pos.x,pos.y, this.width, this.height);
+    }
+  };
